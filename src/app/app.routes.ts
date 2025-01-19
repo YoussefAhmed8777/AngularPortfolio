033028da6699d46f79c2be2598858125df081da6
YoussefAhmed8777 <youssefahmedamin28@gmail.com>
2025-01-19 15:54:47 +0200

    First Verision

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'',redirectTo:"home",pathMatch:'full'},
+  {path:'home',component:HomeComponent,title:'Home',pathMatch:'full'},
+  {path:'about',component:AboutComponent,title:'About',pathMatch:'full'},
+  {path:'portfolio',component:PortfolioComponent,title:'Portfolio',pathMatch:'full'},
+  {path:'contact',component:ContactComponent,title:'Contact',pathMatch:'full'},
+];
