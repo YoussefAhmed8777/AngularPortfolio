b3826bd7b0049f7b825c890bf897973351b407db
YoussefAhmed8777 <youssefahmedamin28@gmail.com>
2025-03-01 21:57:40 +0200

    updated routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,8 +7,7 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes, withHashLocation()),
+    provideRouter(routes, withHashLocation(), withInMemoryScrolling({scrollPositionRestoration:"top"}),),
     provideClientHydration(withEventReplay()),
-    withInMemoryScrolling({scrollPositionRestoration:"top"}),
   ]
 };
